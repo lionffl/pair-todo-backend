357e14857c853edb1bbd091a1af77cf35ceb3b04
lion <lionffl@gmail.com>
2023-01-12 11:31:10 -0300

    fix: cors origins

--- config/envs.rb
+++ config/envs.rb
@@ -1,3 +1,3 @@
 ENV['db_password'] = "mysecretpassword"
 ENV['db_username'] = "postgres"
-ENV['frontend'] = "http://localhost:3001"
+ENV['frontend'] = "https://63c0111ef4708e118932bf1d--sage-dragon-5c5b26.netlify.app"
