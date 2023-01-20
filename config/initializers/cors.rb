6ea5a1164b1503437d80c2ccf232036e89383f7f
lion <lionffl@gmail.com>
2023-01-20 12:25:33 -0300

    cors for dev/prod envs are now ready

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,12 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3001'
+    
+    if Rails.env == 'production'
+      origins 'https://pair-todo.geografialinks.com/'
+    else
+      origins 'http://localhost:3001'
+    end
 
     resource "*",
       headers: :any,
